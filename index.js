ab1cfe3f147eddc254063a90368d5ab384b8ef11
ShashimaniDwivedi <shashimanidwivedi6@gmail.com>
2024-09-03 16:13:07 +0530

    Server  code update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 const hello = "Hello World";
 console.log(hello);
@@ -23,7 +25,8 @@ console.log(hello);
 // console.log("File Written");
 
 //in Asynchronous way
-fs.readFile(
+
+/* fs.readFile(
 	"C:/Users/Manas/Desktop/Node JS/1-node-farm/starter/txt/start.txt",
 	"utf-8",
 	(err, data1) => {
@@ -51,4 +54,41 @@ fs.readFile(
 			}
 		);
 	}
+); */
+/////////////////////////////////
+//Creating Simple Server
+
+const data = fs.readFileSync(
+	"C:/Users/Manas/Desktop/Node JS/1-node-farm/starter/dev-data/data.json",
+	"utf-8"
 );
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+	const pathName = req.url;
+
+	if (pathName === "/" || pathName === "/overview") {
+		res.end("This is the overview page");
+	} else if (pathName === "/product") {
+		res.end("This is the product page");
+	} else if (pathName === "/api") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(data);
+	} else {
+		res.writeHead(404, {
+			"Content-type": "text/html",
+			"my-own-header": "hello-world",
+		});
+		res.end("<h1>Page not found</h1>");
+	}
+});
+
+////////////////////////////////
+//listening request
+server.listen(8000, "127.0.0.1", () => {
+	console.log("Server is listening on port 8000");
+});
+//Routing means Implementing diffrent action for different url
+//Creating Single API
+//API=>It is a request with which we can request some data
